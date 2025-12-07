5f270cb7ae0405c7cc3ec6fd1bc8f16ddb104d00
lucasearom <lucasmoraes211@gmail.com>
2025-12-07 17:57:28 -0300

    feat: created script file

---
+++ src/script/script.js
@@ -0,0 +1,62 @@
+const columns = document.querySelectorAll(".column__cards");
+const cards = document.querySelectorAll(".card");
+
+let draggedCard;
+
+const dragStart = (event) => {
+    draggedCard = event.target;
+    event.dataTransfer.effectAllowed = "move";
+};
+
+const dragOver = (event) => {
+    event.preventDefault();
+};
+
+const dragEnter = ({ target }) => {
+    if (target.classList.contains("column__cards")) {
+        target.classList.add("column--highlight");
+    }
+};
+
+const dragLeave = ({ target }) => {
+    target.classList.remove("column--highlight");
+};
+
+const drop = ({ target }) => {
+    if (target.classList.contains("column__cards")) {
+        target.classList.remove("column--highlight");
+        target.append(draggedCard);
+    }
+};
+
+const createCard = ({ target }) => {
+    if (!target.classList.contains("column__cards")) return;
+    const card = document.createElement("section");
+
+    card.className = "card";
+    card.draggable = "true";
+    card.contentEditable = "true";
+
+    card.addEventListener("focusout", () => {
+        card.contentEditable = "false";
+
+        if (!card.textContent) card.remove();
+    });
+
+    card.addEventListener("dragstart", dragStart);
+
+    target.append(card);
+    card.focus();
+};
+
+cards.forEach((card) => {
+    card.addEventListener("dragstart", dragStart);
+});
+
+columns.forEach((column) => {
+    column.addEventListener("dragover", dragOver);
+    column.addEventListener("dragenter", dragEnter);
+    column.addEventListener("dragleave", dragLeave);
+    column.addEventListener("drop", drop);
+    column.addEventListener("dblclick", createCard);
+});
